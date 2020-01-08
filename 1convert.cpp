8710494d42be51ec85c443bb0e92c7f954efca68
Talbert Herndon <tallypvp@gmail.com>
2020-01-08 14:53:45 -0700

    first commit

---
+++ 1convert.cpp
@@ -0,0 +1,17 @@
+// Talbert Herndon
+// This is my own work.
+// In class exercise 1/8/2020
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Exercise 1. ";
+    cout << "Please enter value of pounds: ";
+    cin >> n;
+    cout << "The value you entered is " << n;
+    cout << " and converted to kilograms it is " << n * 0.454 << ".\n";
+    return 0;
+}
---
+++ 2hexagonarea.cpp
@@ -0,0 +1,21 @@
+// Talbert Herndon
+// This is my own work.
+// In class exercise 1/8/2020
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int s;
+
+    cout << " Exercise 2.";
+    cout << " Please enter number of sides: ";
+    cin >> s;
+    cout << "The number of sides is " << s;
+    cout << " and the area is: " << (3 * sqrt(3)) / 2 * pow(s, 2) << ".\n";
+
+    return 0;
+}
